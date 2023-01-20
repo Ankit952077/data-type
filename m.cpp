bb74672f5bb372efed20c8dfbac11dc33faf3758
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-01-20 12:34:20 -0500

    Add files via upload

---
+++ m.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main ()
+{ //int data type
+int apples =5;
+cout<<apples<<endl;
+cout<<"size of int :"<<sizeof(apples)<<endl;
+
+//char data type
+
+char singh='s';
+cout<<singh<<endl;
+cout<<"size of char:"<<sizeof(singh)<<endl;
+
+// boolean data type
+
+bool flage =true;
+cout<<flage<<endl;
+cout<<"size of bool:"<<sizeof(flage)<<endl;
+
+// floating data type
+
+float score =3.14;
+cout<<score<<endl;
+cout<<"size of float:"<<sizeof(score)<<endl;
+
+//
+
+    return 0;
+
+}
